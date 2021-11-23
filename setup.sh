7f3d61ee3628d34395efa645604dea3db5e791f4
Mofesola Banjo <mofesola.banjo@scoding.com>
2021-11-23 22:12:47 +0200

    Installed symfony for backend, updated setup script

--- setup.sh
+++ setup.sh
@@ -3,4 +3,18 @@
 cp docker-compose-dist.yml docker-compose.yml
 docker network create svsc-bridge
 docker-compose up -d --build
-docker exec -it svsc_nginx bash -c 'cd /app && npm install && npm run build && cp -r /app/build /var/www/html'
+
+printf '\nBUILDING FRONTEND APP'
+cat <<frontend | docker exec --interactive svsc_nginx bash
+  cd /app
+  npm install
+  npm run build
+  cp -r /app/build /var/www/html
+frontend
+
+printf '\nBUILDING BACKEND APP'
+cat <<backend | docker exec --interactive svsc_php bash
+  composer dump-env prod
+  composer install --prefer-dist --no-dev --optimize-autoloader
+  APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+backend
