0cc53a9f4bb243d8d0938db920bde791aab02da1
Mofesola Banjo <mofesola.banjo@scoding.com>
2021-11-25 01:39:53 +0200

    Added migration

---
+++ backend/migrations/Version20211124233755.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211124233755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE searches (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, client_ip VARCHAR(255) NOT NULL, "query" VARCHAR(255) NOT NULL, searched_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE searches');
+    }
+}
