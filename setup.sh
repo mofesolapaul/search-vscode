21a666772dc6df78f89c695419c792af50aa21db
Mofesola Banjo <mofesola.banjo@scoding.com>
2021-11-25 03:43:04 +0200

    Updated build script to run message consumer in the background

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ cp docker-compose-dist.yml docker-compose.yml
 docker network create svsc-bridge
 docker-compose up -d --build
 
-printf '\nBUILDING FRONTEND APP'
+printf '\nBUILDING FRONTEND APP\n'
 cat <<frontend | docker exec --interactive svsc_nginx bash
   cd /app
   npm install
@@ -12,10 +12,11 @@ cat <<frontend | docker exec --interactive svsc_nginx bash
   cp -r /app/build /var/www/html
 frontend
 
-printf '\nBUILDING BACKEND APP'
+printf '\nBUILDING BACKEND APP\n'
 cat <<backend | docker exec --interactive svsc_php bash
   composer dump-env prod
   composer install --prefer-dist --no-dev --optimize-autoloader
   APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
   ./bin/console --no-interaction doctrine:migrations:migrate
+  screen -S messenger -d -m ./bin/console messenger:consume async -vv
 backend
